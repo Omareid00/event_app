fb9c94e868b1cc96ba6339471a48c84519afed46
Omar Eid <omereid829@yahoo.com>
2025-08-16 08:30:47 +0300

    create
    create event

--- lib/cores/appimages/appimages.dart
+++ lib/cores/appimages/appimages.dart
@@ -7,4 +7,12 @@ class AppImages {
   static const String thirdBoarding = "assets/images/thirdboarding.png";
   static const String forgetPassword = "assets/images/change-setting.png";
   static const String Sport = "assets/images/Sport.png";
+  static const String book = "assets/images/Book Club.png";
+  static const String birthday = "assets/images/birthday.png";
+  static const String eating = "assets/images/eating.png";
+  static const String Exhbition = "assets/images/Exhbition.png";
+  static const String holiday = "assets/images/holiday.png";
+  static const String gaming = "assets/images/gaming.png";
+  static const String meeting = "assets/images/meeting.png";
+  static const String workshop = "assets/images/workshop.png";
 }
--- lib/screens/layout/create_event/create_event.dart
+++ lib/screens/layout/create_event/create_event.dart
@@ -1,8 +1,16 @@
 import 'package:event_app/cores/appcolors/appcolors.dart';
+import 'package:event_app/cores/appimages/appimages.dart';
 import 'package:flutter/material.dart';
 
-class CreateEvent extends StatelessWidget {
+class CreateEvent extends StatefulWidget {
   const CreateEvent({Key? key}) : super(key: key);
+
+  @override
+  State<CreateEvent> createState() => _CreateEventState();
+}
+
+class _CreateEventState extends State<CreateEvent> {
+  int selectedTabIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -19,10 +27,46 @@ class CreateEvent extends StatelessWidget {
             color: AppColors.primary
           ),),
       ),
-      body: Column(
-        children: [
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset(AppImages.Sport,fit: BoxFit.cover,)),
+            SizedBox(height: 15,),
+            DefaultTabController(
+              length: categories.length,
+              child: TabBar(
+                isScrollable: true,
+                tabAlignment: TabAlignment.start,
+                padding: EdgeInsets.zero,
+                indicatorColor: AppColors.white,
+                indicator: BoxDecoration(),
+                indicatorPadding: EdgeInsets.symmetric(horizontal: 4),
+                dividerColor: Colors.transparent,
+                labelPadding: EdgeInsets.symmetric(horizontal: 4),
+                onTap: (index) {
+                  setState(() {
+                    selectedTabIndex = index;
+                  });
+                },
 
-        ],
+                tabs: categories.map((categoriesData) {
+                  return Tabitemwidget(
+                    isSelected:
+                    selectedTabIndex ==
+                        categories.indexOf(categoriesData),
+                    categoriesData: categoriesData,
+                  );
+                }).toList(),
+              ),
+            ),
+
+
+
+          ],
+        ),
       ),
     );
   }
